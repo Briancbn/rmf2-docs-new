d4a21cf3018390302d471d546804a06633026051
EthanKuai <ethankuaiek@gmail.com>
2026-07-31 12:21:56 +0800

    feat(sidebar): Logo at sidebar bottom
    Signed-off-by: EthanKuai <ethankuaiek@gmail.com>

---
+++ docs/.vitepress/theme/layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import Footer from './footer.vue'
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #sidebar-nav-after>
+      <img src="/icons/logo-light.svg" class="only-light" />
+      <img src="/icons/logo-dark.svg" class="only-dark" />
+    </template>
+  </Layout>
+</template>
