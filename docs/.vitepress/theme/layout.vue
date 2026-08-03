10789a809b09f519fa73af83483ae342d595d9e7
EthanKuai <ethankuaiek@gmail.com>
2026-08-03 17:27:48 +0800

    refactor(css): Dark theme use isDark()
    Signed-off-by: EthanKuai <ethankuaiek@gmail.com>

--- docs/.vitepress/theme/layout.vue
+++ docs/.vitepress/theme/layout.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
+import { useData } from 'vitepress'
+const { isDark } = useData()
 const { Layout } = DefaultTheme
 </script>
 
 <template>
   <Layout>
     <template #sidebar-nav-after>
-      <img src="/icons/logo-light.svg" class="only-light" />
-      <img src="/icons/logo-dark.svg" class="only-dark" />
+      <img src="/icons/logo-dark.svg" v-if="isDark" />
+      <img src="/icons/logo-light.svg" v-else />
     </template>
   </Layout>
 </template>
